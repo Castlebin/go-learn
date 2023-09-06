cc7367987a625d9f7f05a4bc6c25c09ed437ba0a
Castlebin <Castlebin@gmail.com>
2023-09-06 10:36:06 +0800

    连接 MySQL

--- go-tutorials/Accessing_a_relational_database/data-access/go.mod
+++ go-tutorials/Accessing_a_relational_database/data-access/go.mod
@@ -1,3 +1,5 @@
 module github.com/Castlebin/go-learn/go-tutorials/Accessing_a_relational_database/data-access
 
 go 1.21.0
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
---
+++ go-tutorials/Accessing_a_relational_database/data-access/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:                 "dev",
+		Passwd:               "123456",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3305",
+		DBName:               "recordings",
+		AllowNativePasswords: true,
+	}
+
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
