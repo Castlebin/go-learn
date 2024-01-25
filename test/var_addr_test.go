5d3e1d133ef3edb5b82141e261f6409d0951caff
Castlebin <Castlebin@gmail.com>
2024-01-26 00:26:28 +0800

    理解 go 方法调用参数的值传递和指针传递（引用）

--- test/var_addr_test.go
+++ test/var_addr_test.go
@@ -26,4 +26,33 @@ func Test_var_addr(t *testing.T) {
 	fmt.Printf("结构体变量 aa 的内存地址是：%p\n", &aa)
 }
 
-type AA struct{}
+type AA struct {
+	A int
+}
+
+/*
+从 内存地址的打印结果，看方法传参中 值 和 指针的区别：
+
+可以看到，当传递的是值的时候，方法内部的变量和外部的变量，是两个不同的变量，它们的内存地址是不同的。
+当传递的是指针的时候，方法内部的变量和外部的变量，是同一个变量，它们的内存地址是相同的。
+
+所以，指针传递时，能改变外部变量的值，值传递时，不能改变外部变量的值。
+*/
+func Test_var_addr2(t *testing.T) {
+	a := 1
+	b := AA{1}
+
+	fmt.Printf("变量 a 的内存地址是：%p\n", &a)
+	fmt.Printf("变量 b 的内存地址是：%p\n", &b)
+
+	printAddr(a, &a, b, &b)
+
+	fmt.Print(b)
+}
+
+func printAddr(a int, pa *int, b AA, pb *AA) {
+	fmt.Printf("方法传参 a 的内存地址是：%p\n", &a)
+	fmt.Printf("方法传参 指针 a 地址是：%p\n", pa)
+	fmt.Printf("方法传参 b 的内存地址是：%p\n", &b)
+	fmt.Printf("方法传参 指针 b 地址是：%p\n", pb)
+}
