0813b096be2ec4f1cc49eaf8e223cfc7831cb977
Castlebin <Castlebin@gmail.com>
2024-01-26 01:34:47 +0800

    Stringers
    fmt 包中定义的 Stringer 是使用最普遍的接口之一。
    
    type Stringer interface {
    	String() string
    }
    
    实现 String() 方法时，方法的 receiver 只有是值接收者才会被 fmt.Println 调用！！
    
    go 很搞。。。

--- go-tour/method&interface/stringers.go
+++ go-tour/method&interface/stringers.go
@@ -19,6 +19,7 @@ type Person struct {
 	Age  int
 }
 
+// 这里的 String() 方法是值接收者  ， 只有是值接收者才会被 fmt.Println 调用。
 func (p Person) String() string {
 	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
 }
@@ -27,4 +28,19 @@ func main() {
 	a := Person{"Arthur Dent", 42}
 	z := Person{"Zaphod Beeblebrox", 9001}
 	fmt.Println(a, z)
+
+	// 会发现 打印 Person2 时，并没有调用 String() 方法，而是直接打印了 Person2 的值。
+	b := Person2{"Arthur Dent", 42}
+	y := Person2{"Zaphod Beeblebrox", 9001}
+	fmt.Println(b, y)
+}
+
+type Person2 struct {
+	Name string
+	Age  int
+}
+
+// 区别在于，这里的 String() 方法是指针接收者，而上面的是值接收者。
+func (p2 *Person2) String() string {
+	return fmt.Sprintf("%v (%v years)", p2.Name, p2.Age)
 }
